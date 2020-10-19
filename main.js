15aef2cf7e7aacf8f70a9f0023d083026af667f5
QuackDuster <jjimenez@ucol.mx>
2020-10-19 14:16:26 -0500

    Snakes n Ladders w/ Switch

---
+++ main.js
@@ -0,0 +1,113 @@
+class Dice {
+    constructor(){}
+
+    throwDice(){
+        return Math.ceil(Math.random()*6);
+    }
+}
+
+class Player {
+    constructor(names){
+        this.names=names;
+        this.position=0;
+        this.moved=0;
+    }
+
+    mov(newPos){
+        this.moved= newPos + this.position
+        return this.moved
+        
+    }
+    
+}
+
+class Board {
+    constructor(){
+    }
+
+    verify(section){
+        switch (section){
+            //Escaleras
+            case 5: 
+                return 16;
+            case 9:
+                return 21;
+            case 18:
+                return 30;
+            case 23:
+                return 35;
+            case 31:
+                return 42;
+            case 39:
+                return 51;
+            case 46:
+                return 58;
+            case 56:
+                return 77;
+            // Serpientes
+            case 28: 
+                return 3;
+            case 37:
+                return 29;
+            case 44:
+                return 10;
+            case 70:
+                return 46;
+            case 73:
+                return 64;
+            case 79:
+                return 51;
+            case 87:
+                return 66;
+            case 96:
+                return 53;
+            default: 
+            return section
+        }
+    }
+}
+
+
+let board = new Board
+let dice = new Dice
+
+let P1 = new Player("Quacken")
+let P2 = new Player("Animex")
+
+
+while(P1.position < 100 && P2.position < 100){
+    P1.position = board.verify(P1.mov(dice.throwDice()))
+    P2.position = board.verify(P2.mov(dice.throwDice()))
+
+    console.log(`Posición de J1: ${P1.position} |-| Posición de J2: ${P2.position}`)
+}
+
+if (P1.position >=100){
+    console.log(`\nGanó J1: "${P1.names}"`)
+} else if (P2.position >=100){
+    console.log(`\nGanó J2 "${P2.names}"`)
+}
+
+/*
+var S = 100;
+var Tab = [S];
+for(let i = 0; i<S; i++){
+    Tab[i] = -1;
+}
+
+//ladders
+Tab[2] = 21;
+Tab[4]= 7;
+Tab[10]= 25;
+Tab[19]=28;
+
+//Snakes
+Tab[46]=20;
+Tab[98]=70;
+Tab[22]=2;
+Tab[68]=54;
+
+
+
+console.log(Tab)
+*/
